fdb36ed49264ae2cb3c8ccabe9e24b6f876ebfe0
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-14 00:34:52 +0700

    feat(intro-page): add background color theme to intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -5,6 +5,8 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+    );
   }
 }
