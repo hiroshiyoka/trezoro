0d1a3668e57400b83d64cc40c25734586e629ef8
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-15 01:02:24 +0700

    feat(cart-page): add little bit styling for cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '/components/my_drawer.dart';
+
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Shop Page"),
+      ),
+      drawer: const MyDrawer(),
+      backgroundColor: Theme.of(context).colorScheme.background,
     );
   }
 }
