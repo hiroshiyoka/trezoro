41c6cc3743c874d8d0957c209f7cfe064ff5c4a0
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-18 02:03:33 +0700

    feat(shop): create Shop models List for Products to sale

---
+++ lib/models/shop.dart
@@ -0,0 +1,31 @@
+import 'package:trezoro/models/product.dart';
+
+class Shop {
+  // Products for Sale
+  final List<Product> _shop = [
+    // Product 1
+    Product(
+      name: "Product 1",
+      price: 99.99,
+      description: "Item Description...",
+    ),
+    // Product 2
+    Product(
+      name: "Product 2",
+      price: 99.99,
+      description: "Item Description...",
+    ),
+    // Product 3
+    Product(
+      name: "Product 3",
+      price: 99.99,
+      description: "Item Description...",
+    ),
+    // Product 4
+    Product(
+      name: "Product 4",
+      price: 99.99,
+      description: "Item Description...",
+    ),
+  ];
+}
